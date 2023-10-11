2369827bf8da91a5646f760c555fa5b6b0cfef5f
Flavio F <flavio.filho@hotmail.com>
2023-10-10 22:29:54 -0300

    Limpando projeto e usando template tailwind

---
+++ src/app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>Schedule App</title>
+      <meta name="title" content="Schedule App" />
+      <meta name="description" content="Schedule App" />
+    </>
+  )
+}
