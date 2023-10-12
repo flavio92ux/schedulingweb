0f3c74715037c50e2ac9ecb47e475bdd19f812a5
Flavio F <flavio.filho@hotmail.com>
2023-10-11 21:09:13 -0300

    Mock Auth

--- src/app/head.tsx
+++
@@ -1,9 +0,0 @@
-export default function Head() {
-  return (
-    <>
-      <title>Schedule App</title>
-      <meta name="title" content="Schedule App" />
-      <meta name="description" content="Schedule App" />
-    </>
-  )
-}
